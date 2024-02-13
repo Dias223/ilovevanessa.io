1d124a93e87b259501da14655716a1c22b68a044
Dias223 <66259895+Dias223@users.noreply.github.com>
2024-02-13 21:06:58 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function handleNo() {
+    alert("А я не понял (￣︿￣)?!?!? Мне кажется вы сделали не правильный выбор, попробуйте еще раз (>ω^) ");
+}
+
+function handleYes() {
+    window.location.href = "kiss.html"; // Замените "newpage.html" на путь к новой странице
+}
